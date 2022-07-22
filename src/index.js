2b969ed71516e617e6c0713fc6d121b7d4078dc8
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-07-22 14:37:33 +0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+
+const App = () => {
+    return <h1>Here's our new app!</h1>
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
