a8cecdcbbd99540eef3486f3c79b5b45edbd63e2
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-07-23 21:19:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 import CommentInfo from './CommentInfo'
+import ApprovalCard from "./ApprovalCard";
+import './App.css'
 var faker = require('faker');
 
 
@@ -8,45 +10,47 @@ var faker = require('faker');
 const App = () => {
     return (
         <div className="ui container big comments">
-            <h1>Comments</h1>
-            <CommentInfo 
-                    author="Bolaji"
-                    date="Just now"
-                    text="Hey, this is a great blog post! I like how you broke down all the technicalities."
-                    image={faker.image.image()}
-                    />
-            <CommentInfo 
-                    author="Debbie" 
-                    date="Yesterday at 6:45 PM"
-                    text="Nice post!"
-                    image= {faker.image.image()}
-                    />
-            <CommentInfo 
-                    author="Alex" 
-                    date="Yesterday at 10:15 AM"
-                    text="This was very helpful, thank you!"
-                    image= {faker.image.image()}
-                    />
-            <CommentInfo 
-                    author="Bim" 
-                    date="2 days ago"
-                    text="Can you please write about the new changes in React 18?"
-                    image= {faker.image.image()}
-                    />
-            <CommentInfo 
-                    author="Ekene" 
-                    date="5 days ago"
-                    text="Nice one, boss"
-                    image= {faker.image.image()}
-                    />
-            <form className="ui reply form">
-                    <div className="field">
-                    <textarea></textarea>
-                    </div>
-                    <div className="ui primary submit labeled icon button">
-                    <i className="icon edit"></i> Add Reply
-                    </div>
-            </form>
+          {/* <h1>Comments</h1> */}
+            <ApprovalCard>
+              <CommentInfo 
+                      author="Bolaji"
+                      date="Just now"
+                      text="Hey, this is a great blog post! I like how you broke down all the technicalities."
+                      image={faker.image.image()}
+                      />
+            </ApprovalCard>
+
+        
+            <ApprovalCard>
+                  <CommentInfo 
+                          author="Toria" 
+                          date="Yesterday at 6:45 PM"
+                          text="Nice post!"
+                          image= {faker.image.image()}
+                          />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                  <CommentInfo 
+                          author="Alex" 
+                          date="Yesterday at 10:15 AM"
+                          text="This was very helpful, thank you!"
+                          image= {faker.image.image()}
+                          />
+            </ApprovalCard>
+            
+
+            <ApprovalCard>
+                  <CommentInfo 
+                          author="Bim" 
+                          date="2 days ago"
+                          text="Can you please write about the new changes in React 18?"
+                          image= {faker.image.image()}
+                          />
+            </ApprovalCard>
+            
+            
+
         </div>
         
     )
